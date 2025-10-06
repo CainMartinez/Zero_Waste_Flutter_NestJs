0ffdf751ae773dc26d9c9cd53227e09a48c7cc15
Cain <cainmartinez2@hotmail.com>
2025-10-06 02:04:37 +0200

    Nest JS: Remove unused app and home modules, services, and tests; update Docker Compose for database configuration

--- backend-nest/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return '<h1>Funciona nest</h1><p>Esto es un párrafo de prueba.</p>';
-  }
-}
